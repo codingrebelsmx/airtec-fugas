4d0e84ed706075571bd7682e285698d586c7e7a1
Moises Morales <mmorales.dev@outlook.com>
2019-08-24 21:23:49 -0500

    Se muestran los datos en el dashboard!

--- AirTecFugas/MainApp/templatetags/customFilters.py
+++ AirTecFugas/MainApp/templatetags/customFilters.py
@@ -1,6 +1,8 @@
 from django.template.defaulttags import register
 from django.template.defaultfilters import stringfilter
 from django.contrib.auth.models import Group
+from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 
 @register.filter
@@ -8,4 +10,12 @@ def is_in_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return group in user.groups.all()
 
+
+@register.filter
+def prepend_dollars(dollars):
+    if dollars:
+        dollars = round(float(dollars), 2)
+        return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
+    else:
+        return ''
 
